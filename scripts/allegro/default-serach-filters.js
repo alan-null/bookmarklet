a823b6e12a6033def15b7d0b45ae889e4d897601
Alan Płócieniak <alan-null@outlook.com>
2024-10-05 22:28:31 +0200

    Allegro - default search filters (handle ? and &)

--- scripts/allegro/default-serach-filters.js
+++ scripts/allegro/default-serach-filters.js
@@ -1,1 +1,8 @@
-window.location = window.location + '&stan=nowe&allegro-smart-standard=1&offerTypeBuyNow=1&ocena-produktu=od-4.8&order=qd'
+let currentUrl = window.location.href;
+let separator = '?';
+
+if (currentUrl.includes('?')) {
+    separator = '&';
+}
+
+window.location = window.location + separator + 'stan=nowe&allegro-smart-standard=1&offerTypeBuyNow=1&ocena-produktu=od-4.8&order=qd';
